8341b86530a6131206c6f72b25f3fe2ee0729e1a
Bree Elle Gardner <letsbreelhere@gmail.com>
2016-02-08 18:51:28 -0800

    Start refactoring lexer

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -3,8 +3,9 @@ module Lexer where
 import qualified Text.Megaparsec.Lexer as L
 import           Control.Monad (void)
 import           Text.Megaparsec
-import           Text.Megaparsec.Text
+import           Text.Megaparsec.String
 
+spaceConsumer :: Parser ()
 spaceConsumer = L.space (void spaceChar) (L.skipLineComment "--") (L.skipBlockComment "{-" "-}")
 
 lexeme :: Parser a -> Parser a
@@ -13,32 +14,46 @@ lexeme = L.lexeme spaceConsumer
 symbol :: String -> Parser String
 symbol = L.symbol spaceConsumer
 
-curlyBraces = between (lexeme $ char '{') (lexeme $ char '}')
+curlyBraces :: Parser a -> Parser a
+curlyBraces = between (symbol "{") (symbol "}")
 
-parens = between (lexeme $ char '(') (lexeme $ char ')')
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
 
-squareBraces = between (lexeme $ char '[') (lexeme $ char ']')
+squareBraces :: Parser a -> Parser a
+squareBraces = between (symbol "[") (symbol "]")
 
-keyword = lexeme . string
+keyword :: String -> Parser String
+keyword w = lexeme (string w) <?> ("keyword " ++ show w)
 
-reservedWords = ["while"]
+reservedWords :: [String]
+reservedWords = []
 
+integer :: Parser Integer
 integer = lexeme L.integer <?> "integer"
 
 singleQuote :: Parser Char
 singleQuote = char '\''
+
 doubleQuote :: Parser Char
 doubleQuote = char '"'
 
-stringLiteral = lexeme (doubleQuote *> manyTill L.charLiteral doubleQuote)
+stringLiteral :: Parser String
+stringLiteral = lexeme (doubleQuote *> manyTill L.charLiteral doubleQuote) <?> "string literal"
 
-charLiteral = lexeme (between singleQuote singleQuote L.charLiteral)
+charLiteral :: Parser Char
+charLiteral = lexeme (between singleQuote singleQuote L.charLiteral) <?> "character literal"
 
+identifier :: Parser String
 identifier = (check =<< lexeme baseIdentifier) <?> "identifier"
   where
     baseIdentifier = (:) <$> letterChar <*> many alphaNumChar
     check s = if s `elem` reservedWords
-                then failure [Message $ "Identifier " ++ s ++ " is reserved"]
+                then failWithMessage $ "Identifier `" ++ s ++ "' is reserved"
                 else pure s
 
+semicolon :: Parser String
 semicolon = symbol ";"
+
+failWithMessage :: String -> Parser a
+failWithMessage msg = failure [Message msg]
