2cce0a1b4790df533c0ed73f16b74222abb959f3
Bree Elle Gardner <letsbreelhere@gmail.com>
2016-07-11 23:25:59 -0700

    Basically full-stack compiler tests!
    OK let's be real, this is hella janky. But it makes setup for testing so
    much easier!

--- test/Test/Compiler.hs
+++ test/Test/Compiler.hs
@@ -5,15 +5,25 @@ import qualified Lexer
 import qualified Compiler
 import Test.Tasty
 import Test.Tasty.HUnit
+import GHC.IO.Handle (hPutStr, hClose)
+import System.Exit (ExitCode(..))
+import qualified System.Process as Proc
 
 compilerSpec :: TestTree
 compilerSpec = testGroup "Code generation and compiler"
-  [ testCase "simple return value" $ testCompiler
-      "def main() int [ 42 ]"
-      "; ModuleID = 'Egg!'\n\ndefine i64 @main() {\nentry_a:\n  ret i64 42\n}\n"
+  [ testCase "simple return values" $
+      testInterpreterFile "return42.egg" (ExitFailure 42)
   ]
 
-testCompiler :: String -> String -> Assertion
-testCompiler input expected = do
-    assembly <- Compiler.compile input
-    assembly @?= Right expected
+testInterpreterFile :: FilePath -> ExitCode -> Assertion
+testInterpreterFile path code = do
+  input <- readFile ("test/resources/" ++ path)
+  testInterpreter input code
+
+testInterpreter :: String -> ExitCode -> Assertion
+testInterpreter input expectedCode = do
+  Right assembly <- Compiler.compile input
+  (Just ihdl, _, _, ph) <- Proc.createProcess (Proc.proc "lli-3.5" []){Proc.std_in = Proc.CreatePipe}
+  hPutStr ihdl assembly >> hClose ihdl
+  exitCode <- Proc.waitForProcess ph
+  exitCode @?= expectedCode
